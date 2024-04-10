7d45c56cf9ac6626fd80a30875d91a5c849d9f63
Tabitha Buder <tabibuder@gmail.com>
2024-04-10 16:20:30 -0700

    added plugins for web app

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,21 +9,67 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+    // entry point
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // output for the bundled files
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // dev server configuration 
     plugins: [
-      
+      //generate HTML file
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      // generate a service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // generates a manifest
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('client/src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icons'),
+          }
+        ]
+      })
     ],
 
     module: {
+      //rules for module handling
       rules: [
-        
+        {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
